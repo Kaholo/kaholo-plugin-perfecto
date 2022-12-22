26faf9e19817a329115f5300e197ede76002eec4
xkcm <xkcm16@gmail.com>
2022-12-22 12:40:12 +0100

    Refactor, add linting

--- helpers.js
+++ helpers.js
@@ -6,56 +6,61 @@ const fs = require("fs");
 const exec = util.promisify(childProcess.exec);
 
 async function assertPathExistence(path) {
-    try {
-        await access(path, fs.constants.F_OK);
-    } catch {
-        throw new Error(`Path ${path} does not exist`);
-    }
+  try {
+    await access(path, fs.constants.F_OK);
+  } catch {
+    throw new Error(`Path ${path} does not exist`);
+  }
 }
 
-function handleChildProcess(childProcess, options = {}) {
-    const chunks = [];
-    return new Promise((res, rej) => {
-        const resolver = (code) => {
-            const output = chunks.join("");
-            if (options.verifyExitCode && code !== 0) {
-                rej(new Error(`Code = ${code}\nOutput=${output}`));
-            } else { res(output); }
-        };
+function handleChildProcess(childProcessInstance, options = {}) {
+  const chunks = [];
+  return new Promise((res, rej) => {
+    const resolver = (code) => {
+      const output = chunks.join("");
+      if (options.verifyExitCode && code !== 0) {
+        rej(new Error(`Code = ${code}\nOutput=${output}`));
+      } else {
+        res(output);
+      }
+    };
 
-        childProcess.stdout.on("data", (chunk) => chunks.push(chunk));
-        childProcess.stderr.on("data", (chunk) => chunks.push(chunk));
+    childProcessInstance.stdout.on("data", (chunk) => chunks.push(chunk));
+    childProcessInstance.stderr.on("data", (chunk) => chunks.push(chunk));
 
-        if (options.finishSignal) {
-            childProcess.on(options.finishSignal, resolver);
-        } else { childProcess.on("exit", resolver); }
-        childProcess.on("error", rej);
-    });
+    if (options.finishSignal) {
+      childProcessInstance.on(options.finishSignal, resolver);
+    } else {
+      childProcessInstance.on("exit", resolver);
+    }
+    childProcessInstance.on("error", rej);
+  });
 }
 
 function handleCommonErrors(error) {
-    let message = (error.message || String(error)).toLowerCase();
-    console.info(message)
+  const message = (error.message || String(error)).toLowerCase();
+  console.info(message);
 }
 
-function joinCommand(command) {
-    const output = command.split("\n").map((item) => item.trim()).join(" ; ");
-    return output;
-  }
-  
 async function stopConnect(connect) {
-    if (connect) {
-        const stopConnect = `./perfectoconnect stop`;
-        const proc = childProcess.exec(stopConnect, {});
-        console.log(await handleChildProcess(proc, { verifyExitCode: true }).catch(handleCommonErrors));
-    }
+  if (!connect) {
+    return;
+  }
+
+  const stopConnectCommand = "./perfectoconnect stop";
+  const proc = childProcess.exec(stopConnectCommand, {});
+  const stopOutput = await handleChildProcess(
+    proc,
+    { verifyExitCode: true },
+  ).catch(handleCommonErrors);
+
+  console.info(stopOutput);
 }
 
 module.exports = {
-    assertPathExistence,
-    exec,
-    handleChildProcess,
-    handleCommonErrors,
-    joinCommand,
-    stopConnect
+  assertPathExistence,
+  exec,
+  handleChildProcess,
+  handleCommonErrors,
+  stopConnect,
 };
--- mvn-cli.js
+++ mvn-cli.js
@@ -1,123 +1,132 @@
-const { docker } = require("@kaholo/plugin-library");
 const childProcess = require("child_process");
-const {
-    join: joinPaths,
-    resolve: resolvePath,
-} = require("path");
+const { docker } = require("@kaholo/plugin-library");
 const { homedir: getHomeDirectory } = require("os");
 const {
-    exec,
-    handleChildProcess,
-    handleCommonErrors,
-    assertPathExistence,
-    joinCommand,
-    stopConnect
+  join: joinPaths,
+  resolve: resolvePath,
+} = require("path");
+
+const {
+  exec,
+  handleChildProcess,
+  handleCommonErrors,
+  assertPathExistence,
+  stopConnect,
 } = require("./helpers");
 const {
-    MAVEN_DOCKER_IMAGE,
-    MAVEN_CLI_NAME,
-    MAVEN_CACHE_DIRECTORY_NAME,
+  MAVEN_DOCKER_IMAGE,
+  MAVEN_CLI_NAME,
+  MAVEN_CACHE_DIRECTORY_NAME,
 } = require("./consts.json");
 
 async function execute(params) {
-    let {
-        command,
-        workingDirectory,
-        environmentVariables,
-        secretEnvVars,
-        customImage = MAVEN_DOCKER_IMAGE,
-        connect,
-        jobName,
-        cloudName,
-        securityToken
-    } = params;
+  const {
+    workingDirectory,
+    environmentVariables,
+    secretEnvVars,
+    customImage = MAVEN_DOCKER_IMAGE,
+    connect,
+    jobName,
+    cloudName,
+    securityToken,
+  } = params;
 
-    // if perfecto connect is required
-    if (connect) {
-        await stopConnect(connect);
-        const startConnect =
-            joinCommand(`wget https://downloads.connect.perfectomobile.com/clients/Perfecto-Connect-linux.tar
-        tar -xf Perfecto-Connect-linux.tar
-        ./perfectoconnect start -c ${cloudName}.perfectomobile.com -s ${securityToken}`)
+  let { command } = params;
 
-        const proc = childProcess.exec(startConnect, {});
-        let tunnelLogs = await handleChildProcess(proc, { verifyExitCode: true }).catch(handleCommonErrors);
-        console.info(tunnelLogs)
-        tunnelId = tunnelLogs.trim().split('\n').pop();
-        console.info(`Tunnel ID: ${tunnelId}`)
-        command = `${command} -DtunnelId="${tunnelId}" `;
-    }
+  // if perfecto connect is required
+  if (connect) {
+    const { additionalCommandArguments } = await startConnect({ cloudName, securityToken });
+    command += additionalCommandArguments;
+  }
 
-    const jobNumber = Math.floor(new Date().getTime() / 1000);
-    command = `${command} -B -Dreportium-job-name=${jobName} -Dreportium-job-number=${jobNumber} -DcloudName=${cloudName} -DsecurityToken=${securityToken}`;
+  const jobNumber = Math.floor(Date.now() / 1000);
+  command += ` -B -Dreportium-job-name=${jobName} -Dreportium-job-number=${jobNumber} -DcloudName=${cloudName} -DsecurityToken=${securityToken}`;
 
-    const dockerCommandBuildOptions = {
-        command: docker.sanitizeCommand(command, MAVEN_CLI_NAME),
-        image: customImage,
-    };
+  const dockerCommandBuildOptions = {
+    command: docker.sanitizeCommand(command, MAVEN_CLI_NAME),
+    image: customImage,
+  };
 
-    const mavenAgentCachePath = joinPaths(getHomeDirectory(), MAVEN_CACHE_DIRECTORY_NAME);
-    const mavenCacheVolumeDefinition = docker.createVolumeDefinition(mavenAgentCachePath);
-    // Change mount point to maven cache path
-    mavenCacheVolumeDefinition.mountPoint.value = joinPaths("/root", MAVEN_CACHE_DIRECTORY_NAME);
+  const mavenAgentCachePath = joinPaths(getHomeDirectory(), MAVEN_CACHE_DIRECTORY_NAME);
+  const mavenCacheVolumeDefinition = docker.createVolumeDefinition(mavenAgentCachePath);
+  // Change mount point to maven cache path
+  mavenCacheVolumeDefinition.mountPoint.value = joinPaths("/root", MAVEN_CACHE_DIRECTORY_NAME);
+  const volumeDefinitionsArray = [mavenCacheVolumeDefinition];
 
-    const dockerEnvironmentalVariables = {
-        [mavenCacheVolumeDefinition.mountPoint.name]: mavenCacheVolumeDefinition.mountPoint.value,
-        ...environmentVariables,
-        ...secretEnvVars,
-    };
-    let shellEnvironmentalVariables = {
-        ...dockerEnvironmentalVariables,
-        [mavenCacheVolumeDefinition.path.name]: mavenCacheVolumeDefinition.path.value,
-    };
+  const dockerEnvironmentalVariables = {
+    [mavenCacheVolumeDefinition.mountPoint.name]: mavenCacheVolumeDefinition.mountPoint.value,
+    ...environmentVariables,
+    ...secretEnvVars,
+  };
+  let shellEnvironmentalVariables = {
+    ...dockerEnvironmentalVariables,
+    [mavenCacheVolumeDefinition.path.name]: mavenCacheVolumeDefinition.path.value,
+  };
 
-    const volumeDefinitionsArray = [mavenCacheVolumeDefinition];
+  const absoluteWorkingDirectory = workingDirectory ? resolvePath(workingDirectory) : process.cwd();
+  await assertPathExistence(absoluteWorkingDirectory);
+  const workingDirVolumeDefinition = docker.createVolumeDefinition(absoluteWorkingDirectory);
 
-    const absoluteWorkingDirectory = workingDirectory ? resolvePath(workingDirectory) : process.cwd();
+  dockerEnvironmentalVariables[workingDirVolumeDefinition.mountPoint.name] = (
+    workingDirVolumeDefinition.mountPoint.value
+  );
 
-    await assertPathExistence(absoluteWorkingDirectory);
-    const workingDirVolumeDefinition = docker.createVolumeDefinition(absoluteWorkingDirectory);
+  shellEnvironmentalVariables = {
+    ...shellEnvironmentalVariables,
+    ...dockerEnvironmentalVariables,
+    [workingDirVolumeDefinition.path.name]: workingDirVolumeDefinition.path.value,
+  };
 
-    dockerEnvironmentalVariables[workingDirVolumeDefinition.mountPoint.name] = (
-        workingDirVolumeDefinition.mountPoint.value
-    );
+  volumeDefinitionsArray.push(workingDirVolumeDefinition);
+  dockerCommandBuildOptions.workingDirectory = workingDirVolumeDefinition.mountPoint.value;
 
-    shellEnvironmentalVariables = {
-        ...shellEnvironmentalVariables,
-        ...dockerEnvironmentalVariables,
-        [workingDirVolumeDefinition.path.name]: workingDirVolumeDefinition.path.value,
-    };
+  dockerCommandBuildOptions.volumeDefinitionsArray = volumeDefinitionsArray;
+  dockerCommandBuildOptions.environmentVariables = dockerEnvironmentalVariables;
 
-    volumeDefinitionsArray.push(workingDirVolumeDefinition);
-    dockerCommandBuildOptions.workingDirectory = workingDirVolumeDefinition.mountPoint.value;
+  const dockerCommand = docker.buildDockerCommand(dockerCommandBuildOptions);
 
-    dockerCommandBuildOptions.volumeDefinitionsArray = volumeDefinitionsArray;
-    dockerCommandBuildOptions.environmentVariables = dockerEnvironmentalVariables;
-
-    const dockerCommand = docker.buildDockerCommand(dockerCommandBuildOptions);
-
-    commandOutput = await exec(dockerCommand, {
-        env: shellEnvironmentalVariables,
-    }).catch(async (error) => {
-        await stopConnect(connect);
-        throw new Error(error.toString().replace(securityToken,'--'))
-    });
-
-    if (commandOutput.stderr && !commandOutput.stdout) {
-        await stopConnect(connect);
-        throw new Error(commandOutput.stderr);
-    } else if (commandOutput.stdout) {
-        console.error(commandOutput.stderr);
-    }
-
-    // stops perfecto connect, handling errors and not throwing it as stop command needs to be executed
+  const commandOutput = await exec(dockerCommand, {
+    env: shellEnvironmentalVariables,
+  }).catch(async (error) => {
     await stopConnect(connect);
-    const output = `${commandOutput.stdout}\nReport Link: https://${cloudName}.app.perfectomobile.com/reporting/library?jobName[0]=${jobName}&jobNumber[0]=${jobNumber}`
-    return output;
+    throw new Error(error.toString().replace(securityToken, "--"));
+  });
+
+  // stops perfecto connect, handling errors
+  // and not throwing it as stop command needs to be executed
+  await stopConnect(connect);
+
+  if (commandOutput.stderr && !commandOutput.stdout) {
+    throw new Error(commandOutput.stderr);
+  } else if (commandOutput.stdout) {
+    console.error(commandOutput.stderr);
+  }
+
+  return `${commandOutput.stdout}\nReport Link: https://${cloudName}.app.perfectomobile.com/reporting/library?jobName[0]=${jobName}&jobNumber[0]=${jobNumber}`;
+}
+
+async function startConnect({ cloudName, securityToken }) {
+  await stopConnect(true);
+  const startConnectCommand = [
+    "wget https://downloads.connect.perfectomobile.com/clients/Perfecto-Connect-linux.tar",
+    "tar -xf Perfecto-Connect-linux.tar",
+    `./perfectoconnect start -c ${cloudName}.perfectomobile.com -s ${securityToken}`,
+  ].join(" && ");
+
+  const proc = childProcess.exec(startConnectCommand, {});
+  const tunnelLogs = await handleChildProcess(
+    proc,
+    { verifyExitCode: true },
+  ).catch(handleCommonErrors);
+
+  const tunnelId = tunnelLogs.trim().split("\n").pop();
+
+  console.info(tunnelLogs);
+  console.info(`Tunnel ID: ${tunnelId}`);
+
+  return { additionalCommandArguments: ` -DtunnelId="${tunnelId}" ` };
 }
 
 module.exports = {
-    execute,
+  execute,
 };
-
-
